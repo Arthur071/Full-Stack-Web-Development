3c2628aabbd310ae636a7c94e77774a085e94405
Shadan ijmal <shadanijmal999@gmail.com>
2023-08-30 02:18:42 +0530

    Javascript

---
+++ Java Script/Second.js
@@ -0,0 +1,32 @@
+var a = 78;
+var b = "shadan";
+// console.log(a);
+
+// operator in java script
+// in operation 3+4 "+" is the operator and  3,4 are the operands
+// 1. unary operator - it has single operand (x = -x)
+// examples of unary operator 
+a = -a;
+// console.log(a);
+// 2. binary operator - it has two operand (x = x+4)
+// examples of binary operator
+a=456+9;
+// console.log(a)
+
+
+var num1 = 43;
+var num2 = 98;
+
+console.log('the value of num1 + num2 is '+ (num1 + num2));
+console.log('the value of num1 - num2 is '+ (num1 - num2));
+console.log('the value of num1 * num2 is '+ (num1 * num2));
+console.log('the value of num1 / num2 is '+ (num1 / num2));
+console.log('the value of num1 ** num2 is '+ (num1 ** num2));
+// increment operator
+console.log('the value of num1 ++  is '+ (num1 ++));
+console.log('the value of ++num  is '+ (++num1 ));
+// decreament operator
+console.log('the value of --num  is '+ (--num1 ));
+console.log('the value of num--  is '+ (num1-- ));
+
+

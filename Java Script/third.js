1ec17f26ba8fe551ea1ad09670f965a9d02d6e64
Shadan ijmal <shadanijmal999@gmail.com>
2023-09-01 02:17:11 +0530

    Javascript

---
+++ Java Script/third.js
@@ -0,0 +1,11 @@
+var string = "this's";
+var name = "Shadan";
+var channel = "clasher";
+var message = "he is rizzgod";
+var temp = `${name} is a nice person and  he has channel called ${channel}`
+// console.log(temp)
+// var len =  name.length;
+// console.log(`length of the name is ${len}`)
+var y =  new String("this");
+console.log(y)
+document.getElementById('content').innerHTML = '<h3> this is an  h3 heading </h3>'
